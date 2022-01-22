fdd42dddfa213974fd4f2dc3e6f8f83cca7b48a2
console.log(cat == cute) <46598063+qwreey75@users.noreply.github.com>
2022-01-22 20:57:40 +0900

    Create package.lua

---
+++ package.lua
@@ -0,0 +1,11 @@
+return {
+	name = 'qwreey75/mutex',
+	version = '1.0.3',
+	homepage = 'https://github.com/qwreey75/mutex.lua',
+	dependencies = {
+	},
+	tags = {'coroutine','blocking','async','mutex'},
+	license = 'MIT',
+	author = 'qwreey75',
+	files = {'**.lua'},
+}
