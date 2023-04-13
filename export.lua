e99565a242b8db306d088af94f2cf77ab30960f5
Qwreey <qwreey75@gmail.com>
2023-04-14 02:32:10 +0900

    Change namespaces, luau types

---
+++ export.lua
@@ -0,0 +1,17 @@
+export type id = string
+export type mutex = {
+	ID: id?,
+	New: (id:string?)->mutex;
+	Wait: (self:mutex,thisThread:thread?)->(),
+	Lock: (self:mutex,thisThread:thread?)->(),
+	Unlock: (self:mutex)->(),
+	IsLocked: (self:mutex)->boolean,
+	Destroy: (self:mutex,force:boolean?)->(),
+}
+export type module = {
+	New: (id:string?)->mutex;
+	MutexList: {[id]: mutex};
+}
+export type init = ()->module
+
+return require(script.spring) :: module & { Init: init }
